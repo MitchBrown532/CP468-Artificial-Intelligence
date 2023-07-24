1cfe24c975fb879cf7a7608e1536e62e7e1a6336
blakephillipsss <blake.kingfield@gmail.com>
2023-07-24 14:49:57 -0400

    Functioning untrained model

---
+++ Final/src/model.py
@@ -0,0 +1,66 @@
+import numpy as np
+import os
+from sklearn.model_selection import train_test_split
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Dropout, Flatten
+from tensorflow.keras.layers import Conv2D, MaxPooling2D
+from tensorflow.keras.losses import categorical_crossentropy
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.utils import to_categorical
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+
+script_dir = os.path.dirname(os.path.abspath(__file__))
+
+train_dir = os.path.join(script_dir, '../data/preprocessed/train')
+test_dir = os.path.join(script_dir, '../data/preprocessed/test')
+
+train_datagen = ImageDataGenerator(rescale=1./255,
+                                   rotation_range=30,
+                                   width_shift_range=0.1,
+                                   height_shift_range=0.1,
+                                   shear_range=0.2,
+                                   zoom_range=0.2,
+                                   horizontal_flip=True,
+                                   fill_mode='nearest')
+
+test_datagen = ImageDataGenerator(rescale=1./255)
+
+train_generator = train_datagen.flow_from_directory(train_dir,
+                                                    color_mode='grayscale',
+                                                    target_size=(48,48),
+                                                    batch_size=64,
+                                                    class_mode='categorical',
+                                                    shuffle=True)
+
+test_generator = test_datagen.flow_from_directory(test_dir,
+                                                  color_mode='grayscale',
+                                                  target_size=(48,48),
+                                                  batch_size=64,
+                                                  class_mode='categorical',
+                                                  shuffle=False)
+
+
+model = Sequential()
+
+model.add(Conv2D(64, kernel_size=(3, 3), activation='relu', input_shape=(48,48,1)))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+model.add(Conv2D(128, kernel_size=(3, 3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+model.add(Flatten())
+model.add(Dense(1024, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(7, activation='softmax'))
+
+
+model.compile(loss='categorical_crossentropy',
+              optimizer=Adam(),
+              metrics=['accuracy'])
+
+
+model.fit(train_generator,
+          steps_per_epoch=int(0.1*train_generator.n//train_generator.batch_size),
+          epochs=30,
+          validation_data=test_generator,
+          validation_steps=test_generator.n//test_generator.batch_size)
